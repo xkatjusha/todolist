9c7a1fc912943edb858a552aa3556da64c340ca3
xkatjusha <katharina.maier.25@web.de>
2022-12-22 13:31:06 +0100

    edit app component + add todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      TodoComponent
+   ],
   imports: [
     BrowserModule
   ],
